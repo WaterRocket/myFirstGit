ddd9203ff3ea0388e98a75468884e2c662c7abd3
Jason Wicks <135831726+WaterRocket@users.noreply.github.com>
2023-07-28 14:56:50 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function adding(one, two){
     console.log(one+two); 
 }
@@ -6,6 +5,3 @@ function adding(one, two){
 console.log("hello world"); 
 
 adding(5, 10); 
-=======
-console.log("I caused a merge conflict."); 
->>>>>>> 4d54cd79370e2c153bc3c8e4158c0e88c37ba259
